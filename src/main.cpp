c2399802a95509e203b6712353e06b4d84f3374b
introsent <161451906+introsent@users.noreply.github.com>
2025-03-12 13:48:45 +0100

    feat: make window resizable

--- src/main.cpp
+++ src/main.cpp
@@ -144,7 +144,7 @@ private:
         glfwInit();
 
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        //glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
         glfwSetWindowUserPointer(window, this);
