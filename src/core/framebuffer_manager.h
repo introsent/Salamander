e6ceb2b1648f350d7b4f22372a524bde5e33378a
Ivans Minajevs <161451906+introsent@users.noreply.github.com>
2025-04-11 16:44:09 +0200

    feat: apply rule of 5 on swapchain, image views and framebuffer manager

--- src/core/framebuffer_manager.h
+++ src/core/framebuffer_manager.h
@@ -10,6 +10,10 @@ public:
         VkRenderPass renderPass,
         VkImageView depthImageView);
     ~FramebufferManager();
+    FramebufferManager(const FramebufferManager&) = delete;
+    FramebufferManager& operator=(const FramebufferManager&) = delete;
+    FramebufferManager(FramebufferManager&&) = delete;
+    FramebufferManager& operator=(FramebufferManager&&) = delete;
 
     const std::vector<VkFramebuffer>& framebuffers() const { return m_framebuffers; }
     void recreate(const std::vector<VkImageView>* imageViews, VkExtent2D extent,
