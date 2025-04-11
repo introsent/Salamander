f75ff5eae74a044f934ccb9426fb7a92951c75b2
Ivans Minajevs <161451906+introsent@users.noreply.github.com>
2025-04-11 14:35:37 +0200

    feat: encapsulate swap chain

---
+++ src/core/swap_chain.h
@@ -0,0 +1,42 @@
+// swap_chain.h
+#pragma once
+#include "context.h"
+#include <vector>
+
+struct SwapChainSupportDetails {
+    VkSurfaceCapabilitiesKHR capabilities;
+    std::vector<VkSurfaceFormatKHR> formats;
+    std::vector<VkPresentModeKHR> presentModes;
+};
+
+class SwapChain final {
+public:
+    SwapChain(Context& context, Window& window);
+    ~SwapChain();
+
+    void recreate();
+    VkExtent2D extent() const { return m_extent; }
+    VkFormat format() const { return m_imageFormat; }
+    const std::vector<VkFramebuffer>& framebuffers() const { return m_framebuffers; }
+
+private:
+    void createSwapChain();
+    void createImageViews();
+    void createFramebuffers(VkRenderPass renderPass, VkImageView depthImageView);
+    void cleanup();
+
+   SwapChainSupportDetails querySwapChainSupport() const;
+    static VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+    static VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
+    VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities) const;
+
+
+    Context& m_context;
+    Window& m_window;
+    VkSwapchainKHR m_swapChain = VK_NULL_HANDLE;
+    VkFormat m_imageFormat;
+    VkExtent2D m_extent;
+    std::vector<VkImage> m_images;
+    std::vector<VkImageView> m_imageViews;
+    std::vector<VkFramebuffer> m_framebuffers;
+};
