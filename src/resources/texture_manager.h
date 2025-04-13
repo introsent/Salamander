b4b9a829a8b19294608b6a4efd53d465edebff7f
Ivans Minajevs <161451906+introsent@users.noreply.github.com>
2025-04-13 20:54:01 +0200

    fix: do not delete vma image manually when resizing the window

--- src/resources/texture_manager.h
+++ src/resources/texture_manager.h
@@ -1,24 +1,24 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
-#include <string>
-#include <vector>
-
 #include "vk_mem_alloc.h"
+#include <vector>
+#include <string>
 
-class CommandManager;
 class BufferManager;
+class CommandManager;
 
 struct ManagedTexture {
     VkImage image = VK_NULL_HANDLE;
     VmaAllocation allocation = nullptr;
     VkImageView view = VK_NULL_HANDLE;
-    VkSampler sampler = VK_NULL_HANDLE; // optional, null for non-sampled textures
+    VkSampler sampler = VK_NULL_HANDLE; // Optional (only for sampled images)
 };
 
 class TextureManager {
 public:
-    TextureManager(VkDevice device, VmaAllocator allocator, CommandManager* commandManager, BufferManager* bufferManager);
+    TextureManager(VkDevice device, VmaAllocator allocator,
+        CommandManager* commandManager, BufferManager* bufferManager);
     ~TextureManager();
     TextureManager(const TextureManager&) = delete;
     TextureManager& operator=(const TextureManager&) = delete;
@@ -26,21 +26,13 @@ public:
     TextureManager& operator=(TextureManager&&) = delete;
 
     ManagedTexture& loadTexture(const std::string& filepath);
-    ManagedTexture& createTexture(uint32_t width, uint32_t height, VkFormat format, VkImageUsageFlags usage,
-                                  VmaMemoryUsage memoryUsage, VkImageAspectFlags aspect, bool createSampler);
+    ManagedTexture& createTexture(uint32_t width, uint32_t height, VkFormat format,
+        VkImageUsageFlags usage, VmaMemoryUsage memoryUsage,
+        VkImageAspectFlags aspect, bool createSampler = false);
 
     const std::vector<ManagedTexture>& getTextures() const { return m_managedTextures; }
 
 private:
-    void createImage(uint32_t width, uint32_t height, VkFormat format,
-        VkImageTiling tiling, VkImageUsageFlags usage,
-        VmaMemoryUsage memoryUsage, VkImage& image, VmaAllocation& allocation) const;
-
-    void transitionImageLayout(VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout) const;
-    void copyBufferToImage(VkBuffer buffer, VkImage image, uint32_t width, uint32_t height) const;
-    VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags) const;
-    void createSampler();
-
     VkDevice m_device;
     VmaAllocator m_allocator;
     CommandManager* m_commandManager;
@@ -48,4 +40,15 @@ private:
 
     std::vector<ManagedTexture> m_managedTextures;
 
+    void createImage(uint32_t width, uint32_t height, VkFormat format,
+        VkImageTiling tiling, VkImageUsageFlags usage,
+        VmaMemoryUsage memoryUsage, VkImage& image, VmaAllocation& allocation) const;
+
+    VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags) const;
+    VkSampler createSampler() const;
+
+    void transitionImageLayout(VkImage image, VkFormat format,
+        VkImageLayout oldLayout, VkImageLayout newLayout) const;
+    void copyBufferToImage(VkBuffer buffer, VkImage image,
+        uint32_t width, uint32_t height) const;
 };
