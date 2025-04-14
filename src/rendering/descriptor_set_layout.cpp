899dac4a03fd483db078a275a67da80cfb759d59
introsent <161451906+introsent@users.noreply.github.com>
2025-04-14 12:40:05 +0200

    feat: encapsulate descriptor set layout

---
+++ src/rendering/descriptor_set_layout.cpp
@@ -0,0 +1,37 @@
+#include "descriptor_set_layout.h"
+#include <array>
+#include <stdexcept>
+
+DescriptorSetLayout::DescriptorSetLayout(const Context* context) : m_context(context) {
+    createLayout();
+}
+
+DescriptorSetLayout::~DescriptorSetLayout() {
+    vkDestroyDescriptorSetLayout(m_context->device(), m_layout, nullptr);
+}
+
+void DescriptorSetLayout::createLayout() {
+    VkDescriptorSetLayoutBinding uboLayoutBinding{};
+    uboLayoutBinding.binding = 0;
+    uboLayoutBinding.descriptorCount = 1;
+    uboLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    uboLayoutBinding.pImmutableSamplers = nullptr;
+    uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+
+    VkDescriptorSetLayoutBinding samplerLayoutBinding{};
+    samplerLayoutBinding.binding = 1;
+    samplerLayoutBinding.descriptorCount = 1;
+    samplerLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+    samplerLayoutBinding.pImmutableSamplers = nullptr;
+    samplerLayoutBinding.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
+
+    std::array<VkDescriptorSetLayoutBinding, 2> bindings = { uboLayoutBinding, samplerLayoutBinding };
+    VkDescriptorSetLayoutCreateInfo layoutInfo{};
+    layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+    layoutInfo.bindingCount = static_cast<uint32_t>(bindings.size());
+    layoutInfo.pBindings = bindings.data();
+
+    if (vkCreateDescriptorSetLayout(m_context->device(), &layoutInfo, nullptr, &m_layout) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create descriptor set layout!");
+    }
+}
---
+++ src/rendering/descriptor_set_layout.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "../core/context.h"
+
+class DescriptorSetLayout final {
+public:
+    DescriptorSetLayout(const Context* context);
+    ~DescriptorSetLayout();
+    DescriptorSetLayout(const DescriptorSetLayout&) = delete;
+    DescriptorSetLayout& operator=(const DescriptorSetLayout&) = delete;
+    DescriptorSetLayout(DescriptorSetLayout&&) = delete;
+    DescriptorSetLayout& operator=(DescriptorSetLayout&&) = delete;
+
+    VkDescriptorSetLayout handle() const { return m_layout; }
+
+private:
+    const Context* m_context;
+    VkDescriptorSetLayout m_layout;
+
+    void createLayout();
+};
