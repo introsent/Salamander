1e49401828a87ba5e9c93e910979940ccd557a1a
Ivans Minajevs <161451906+introsent@users.noreply.github.com>
2025-11-30 23:25:30 +0100

    feat: load indices and material index per primitive

--- src/resources/loaders/assimp_loader.cpp
+++ src/resources/loaders/assimp_loader.cpp
@@ -28,7 +28,6 @@ bool AssimpLoader::LoadFromFile(const std::string& path, GLTFModel& outModel)
     for (unsigned int meshInx = 0 ; meshInx < scene->mNumMeshes ; meshInx++) {
         const aiMesh* mesh = scene->mMeshes[meshInx];
 
-
         const aiVector3D zero3D (0.0f, 0.0f, 0.0f);
         for (unsigned int vertexInx = 0 ; vertexInx < mesh->mNumVertices ; vertexInx++)
         {
@@ -44,12 +43,37 @@ bool AssimpLoader::LoadFromFile(const std::string& path, GLTFModel& outModel)
             vertex.tangent = glm::vec4(tangent->x, tangent->y, tangent->z, 1);
             vertex.texCoord = glm::vec2(texCoord->x, texCoord->y);
 
+            // Fill vertices vector
             outModel.vertices.push_back(vertex);
         }
+
+        for (unsigned int faceInx = 0; faceInx < mesh->mNumFaces; faceInx++)
+        {
+            const aiFace& face = mesh->mFaces[faceInx];
+
+            if (face.mNumIndices != 3)
+            {
+                std::cerr << "Number of indices must be 3" << std::endl;
+            }
+
+            // Fill indices vector
+            outModel.indices.push_back(face.mIndices[0]);
+            outModel.indices.push_back(face.mIndices[1]);
+            outModel.indices.push_back(face.mIndices[2]);
+        }
+
+        GLTFPrimitive primitive {};
+        primitive.materialIndex = mesh->mMaterialIndex;
+        outModel.primitives.push_back(primitive);
     }
 
 
-    //DoTheSceneProcessing( scene);
+    //for (unsigned int textureInx = 0; textureInx < scene->mNumTextures; textureInx++)
+    //{
+    //    const aiTexture* texture = scene->mTextures[textureInx];
+    //    outModel.textures.push_back ( {  .uri =  texture->mFilename.C_Str() });
+    //}
+
 
     // We're done. Everything will be cleaned up by the importer destructor
     return true;
